ff60b4941fccefb5585d108c6074c93bab6bec6e
Nilesssh <103926034+Nilesssh@users.noreply.github.com>
2023-06-16 17:02:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let button = document.querySelector("button");
+let para = document.querySelector("#status");
+button.addEventListener("click",()=>{
+	para.innerHTML = "";
+	para.insertAdjacentHTML("beforeend","<h1>Entered Metaverse</h1>");
+})
